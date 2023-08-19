7928f15fd25cc2a54ae0b9b7fc054ed7d080c191
HOXUANPHU <phusuper68@gmail.com>
2023-08-19 18:05:10 +0700

    pcf8575_pro

---
+++ pcf8575_pro/PCF8575_pro.h
@@ -0,0 +1,110 @@
+#ifndef __PCF8575_H__
+#define __PCF8575_H__
+
+#include <stddef.h>
+#include <i2cdev.h>
+#include <esp_err.h>
+#include "driver/gpio.h"
+#include "esp_idf_lib_helpers.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define LLL_ADDRESS 0x40    // A[2]A[1]A[0] = 000  
+#define LLH_ADDRESS 0X42    // A[2]A[1]A[0] = 001   
+#define LHL_ADDRESS 0X44    // A[2]A[1]A[0] = 010 
+#define LHH_ADDRESS 0X46    // A[2]A[1]A[0] = 011 
+#define HLL_ADDRESS 0X48    // A[2]A[1]A[0] = 100 
+#define HLH_ADDRESS 0X4A    // A[2]A[1]A[0] = 101 
+#define HHL_ADDRESS 0x4C    // A[2]A[1]A[0] = 110 
+#define HHH_ADDRESS 0x4E    // A[2]A[1]A[0] = 111 
+
+#define PCF8575_INTERRUPT_PIN GPIO_NUM
+
+typedef struct {
+    uint16_t io_state;
+    int gpio_interrupt;
+    i2c_dev_t *dev ;
+    uint8_t address ;
+} pcf8575_t ;
+
+/**
+ * @brief Initalize device descriptor
+ * @param pcf8575 Struct of pcf8575
+ * @param addr Address of I2C (0x0100[A2][A1][A0][R/W] for pcf8575])
+ * @param port Port I2C
+ * @param scl GPIO set up scl
+ * @param sda GPIO set up sda 
+ * @return ESP_OK 
+ * 
+*/
+esp_err_t pcf8575_initialize(pcf8575_t *pcf8575, uint8_t addr, i2c_dev_t port, gpio_config_t scl, gpio_config_t sda);
+
+/**
+ * @brief Free device descriptor
+ * @param pcf8575 Struct of pcf8575
+ * @return ESP_OK
+ */
+esp_err_t pcf8575_deinitialize(pcf8575_t *pcf8575);
+
+/**
+ * @brief Read All GPIO port value
+ * @param pcf8575 Struct of pcf8575
+ * @param val 16-bit GPIO port value
+ * @return ESP_OK
+ */
+esp_err_t pcf8575_readAllPin(pcf8575_t *pcf8575, uint16_t *val);
+
+/**
+ * @brief Write All GPIO port value
+ * @param pcf8575 Struct of pcf8575
+ * @param val 16-bit GPIO port value
+ * @return ESP_OK
+*/
+esp_err_t pcf8575_writeAllPin(pcf8575_t *pcf8575, uint16_t val);
+
+/**
+ * @brief Read GPIO value
+ * @param pcf8575 Struct of pcf8575
+ * @param pin Number of GPIO
+ * @param state logic 0 or logic 1
+ * @return ESP_OK
+*/
+esp_err_t pcf8575_readOnePin(pcf8575_t *pcf8575, uint16_t pin, bool *state);
+
+/**
+ * @brief Write GPIO value
+ * @param pcf8575 Struct of pcf8575
+ * @param pin Number of GPIO
+ * @param state logic 0 or logic 1
+ * @return ESP_OK
+*/
+esp_err_t pcf8575_writeOnePin(pcf8575_t *pcf8575, uint16_t pin, bool state);
+
+/**
+ * @brief Funciton Interrupt
+ * @param pcf8575 Struct of pcf8575
+ * @param enable enable interrupt or prevent interrupt
+*/
+esp_err_t pcf8575_interrupt(pcf8575_t *pcf8575, bool enable);
+
+/**
+ * @brief Check interrupt occurred
+ * @param pcf8575 Struct of pcf8575
+ * @param occurred False(interrupt didn't occur) , True(interrup occurred)
+*/
+esp_err_t pcf8575_interruptCheck(pcf8575_t *pcf8575, bool *occurred);
+
+/**
+ * @brief Clear flag interrupt
+ * @param pcf8575 Struct of pcf8575
+*/
+esp_err_t pcf8575_interruptClear(pcf8575_t *pcf8575);
+
+#ifdef __cplusplus
+}
+#endif
+/**@}*/
+
+#endif /* __PCF8575_H__ */
---
+++ pcf8575_pro/PCF8585_pro.c
@@ -0,0 +1,124 @@
+#include "PCF8575_pro.h"
+#include "pcf8575.h"
+
+//----------------------****----------------------------------------------------------
+// Initialize PCF8575
+esp_err_t pcf8575_initialize(pcf8575_t *pcf8575, uint8_t addr, i2c_dev_t port, gpio_config_t scl, gpio_config_t sda){
+    
+    esp_err_t error_code = pcf8575_init_decs(pcf8575->dev, addr, port , scl, sda);
+        if (error_code != ESP_OK) {
+            ESP_LOGI(__func__,"pcf8575 initialize failed !");
+            return error_code ;
+        }
+        else {
+            ESP_LOGI(__func__,"pcf8575 initialize successfully !");
+            pcf8575_port_write(pcf8575->dev,0xFFFF);
+            return error_code;
+        }
+}
+// 
+esp_err_t pcf8575_deinitialize(pcf8575_t *pcf8575){
+    esp_err_t error_code = pcf8575_free_desc(pcf8575->dev);
+    if(error_code != ESP_OK){
+        ESP_LOGI(__func__,"pcf8575 deinitialize failed !")
+        return error_code;
+    }
+    else {
+        ESP_LOGI(__func__,"pcf8575 deinitialize successfully !")
+        return error_code;
+    }
+}
+
+esp_err_t pcf8575_readAllPin(pcf8575_t *pcf8575, uint16_t *val) 
+{
+    esp_err_t error_code = pcf8575_port_read(pcf8575->dev,pcf8575->io_state);
+    if(error_code != ESP_OK){
+        ESP_LOGI(__func__,"pcf8575 read all pin failed !");
+        return error_code;
+    }
+    else{
+        ESP_LOGI(__func__,"pcf8575 read all pin successfully !");
+        return error_code;
+    }
+}
+
+
+esp_err_t pcf8575_writeAllPin(pcf8575_t *pcf8575 ,uint16_t val){
+
+    esp_err_t error_code = pcf8575_port_write(pcf8575->dev,val);
+    if(error_code != ESP_OK){
+        ESP_LOGI(__func__,"pcf8575 write all pin failed !");
+        return error_code;
+    }
+    else{
+        ESP_LOGI(__func__,"pcf8575 write all pin successfully !");
+        return error_code;
+    }
+}
+
+esp_err_t pcf8575_writeOnePin(pcf8575_t *pcf8575, uint8_t pin, bool state){
+
+    
+    if(pin > 15) return ESP_ERR_INVALID_ARG;
+
+    uint16_t data = pcf8575->io_state ;
+    if (state) data |= BV(pin);
+    else data &= ~ BV(pin);
+
+    return pcf8575_writeAllPin(pcf8575,data);
+}
+
+esp_err_t pcf8575_readOnePin(pcf8575_t *pcf8575, uint16_t pin, bool &state){
+    if(pin > 15) return ESP_ERR_INVALID_ARG;
+
+    uint16_t data = pcf8575->io_state ;
+    *state = data & BV(pin);
+    return ESP_OK;
+}
+
+
+void IRAM_ATTR interrupe_handler(void *arg){
+    printf("Interrupt occupied!\n");
+}
+
+esp_err_t pcf8575_interrupt(pcf8575_t *pcf8575,bool enable){
+    if (enable){
+        // Cấu hình GPIO làm đầu vào với chế độ pull-pu là HIGH
+        gpio_config_t io_conf;
+        io_conf.intr_type = GPIO_INTR_NEGEDGE ; // Kích hoạt interrupt khi GPIO thay đổi
+        io_conf.pin_bit_mask = (1ULL << PCF8575_INTERRUPT_PIN);
+        io_conf.mode = GPIO_MODE_INPUT;
+        io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+        gpio_config(&io_conf);
+        
+        // Hàm xử lý khi interrupt xảy ra
+        gpio_isr_handler_add(PCF8575_INTERRUPT_PIN,interrupe_handler,NULL);
+    }
+    else {
+        gpio_intr_disable(PCF8575_INTERRUPT_PIN);
+    }
+return ESP_OK;
+}
+
+esp_err_t pcf8575_interruptCheck(pcf8575_t *pcf8575, bool *occurred){
+    if(!occurred){
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    int level = gpio_get_level(PCF8575_INTERRUPT_PIN);
+
+    *occurred = (level == 0) ;
+    return ESP_OK;
+}
+
+esp_err_t pcf8575_interruptClear(pcf8575_t *pcf8575) {
+    uint16_t data;
+
+    // Đọc trạng thái hiện tại của các chân I/O để xóa cờ interrupt
+    esp_err_t ret = pcf8575_read(pcf8575->dev, &pcf8575->io_state);
+    if (ret != ESP_OK) {
+        return ret;
+    }
+
+    return ESP_OK;
+}
